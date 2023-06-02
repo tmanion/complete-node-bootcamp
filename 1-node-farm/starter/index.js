75a260b69e892a57a6a040de08ed0a1002dc3fe3
Tony Manion <tonymanion@live.com>
2023-06-02 16:10:27 -0500

    Update index.js
    Lecture 2.11

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,8 @@
-const fs = require("fs");
+// const fs = require("fs");
+const http = require("http");
 
+////////////////////////////////
+// FILES
 // blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -8,18 +11,30 @@ const fs = require("fs");
 // console.log('File written!');
 
 // non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    if(err) return console.log('ERROR! 💥')
 
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if(err) return console.log('ERROR! 💥')
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your file has been written 😁');
-            });
-        });
-    });
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3);
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written 😁');
+//             });
+//         });
+//     });
+// });
+// console.log('Will read file!');
+
+////////////////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!');
 });
-console.log('Will read file!');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+  });
